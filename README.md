f6f3183765349e311f8db6b3256fce7da5ae8055
Jonas Hecht <jonas.hecht@codecentric.de>
2017-03-16 14:27:04 +0100

    With dynamic route discovery through the usage of Eureka, we can delete the route definition :)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # spring-cloud-netflix-docker
 Example project combining Spring Boot apps together with Spring Cloud Netflix &amp; Docker
+
+
+### Links
+
+https://github.com/sqshq/PiggyMetrics
+
+https://github.com/kbastani/spring-cloud-microservice-example
--- zuul-edgeservice/src/main/java/de/jonashackt/edge/ZuulEdgeserviceApplication.java
+++ zuul-edgeservice/src/main/java/de/jonashackt/edge/ZuulEdgeserviceApplication.java
@@ -3,11 +3,13 @@ package de.jonashackt.edge;
 import de.jonashackt.edge.filters.pre.LoggingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulEdgeserviceApplication {
 
 	public static void main(String[] args) {
