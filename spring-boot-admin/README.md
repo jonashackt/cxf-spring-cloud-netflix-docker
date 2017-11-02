9a41d30479164753649046ec60cab129c1fc2780
Jonas Hecht <jonas.hecht@codecentric.de>
2017-11-02 22:00:43 +0100

    Added Spring-Boot-Admin No. 2

---
+++ spring-boot-admin/README.md
@@ -0,0 +1,2 @@
+# spring-boot-admin
+[Spring-Boot-Admin](https://github.com/codecentric/spring-boot-admin) instance to show it´s capabilities.
---
+++ spring-boot-admin/src/main/java/de/jonashackt/admin/SpringBootAdminApplication.java
@@ -0,0 +1,19 @@
+package de.jonashackt.admin;
+
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringBootAdminApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+}
+
