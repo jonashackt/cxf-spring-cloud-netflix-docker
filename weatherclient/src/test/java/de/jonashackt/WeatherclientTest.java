5b4e434a17ed30b17b02ac56185457f9c463a39a
Jonas Hecht <jonas.hecht@codecentric.de>
2020-11-03 08:38:27 +0100

    Also dynamic port for weatherclient

--- weatherclient/src/test/java/de/jonashackt/WeatherclientTest.java
+++ weatherclient/src/test/java/de/jonashackt/WeatherclientTest.java
@@ -1,10 +1,13 @@
 package de.jonashackt;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
@@ -12,10 +15,18 @@ import static io.restassured.RestAssured.given;
 @RunWith(SpringRunner.class)
 @SpringBootTest(
 		classes = WeatherclientTestApplication.class,
-		webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
-		properties = {"server.port=8087"}
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
 public class WeatherclientTest {
+
+	@LocalServerPort
+	int port;
+
+	@Before
+	public void init() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
 
 	@Test public void
 	should_retrieve_forecast_from_weatherservice() {
@@ -24,7 +35,7 @@ public class WeatherclientTest {
 			.contentType(ContentType.JSON)
 			.pathParam("zip", "99425")
 		.when()
-			.get("http://localhost:8087/forecast/{zip}")
+			.get("/forecast/{zip}")
 		.then()
 			.statusCode(HttpStatus.SC_OK);
 	}
